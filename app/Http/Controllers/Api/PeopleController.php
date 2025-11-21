64c7fdac1ffe15f75b75b22b9be113d10713f982
khrsman <khrsman@gmail.com>
2025-11-21 16:17:36 +0700

    change the output of the pictures as an array of url, not array of object

--- app/Http/Controllers/Api/PeopleController.php
+++ app/Http/Controllers/Api/PeopleController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePeopleRequest;
 use App\Http\Requests\UpdatePeopleRequest;
 use App\Models\People;
+use App\Http\Resources\PeopleResource;
 use Illuminate\Http\Request;
 
 class PeopleController extends Controller
@@ -26,13 +27,16 @@ class PeopleController extends Controller
             $query->where('location', '<=', (int) $request->get('max_distance'));
         }
 
-        return response()->json($query->paginate(20));
+        $paginator = $query->paginate(20);
+        $paginator->getCollection()->transform(fn ($item) => new PeopleResource($item));
+        return response()->json($paginator);
     }
 
     public function store(StorePeopleRequest $request)
     {
         $people = People::create($request->validated());
-        return response()->json($people, 201);
+        $people->load(['pictures' => function ($q) { $q->orderBy('sort_order'); }]);
+        return response()->json(new PeopleResource($people), 201);
     }
 
     public function show(People $people)
@@ -40,13 +44,14 @@ class PeopleController extends Controller
         $people->load(['pictures' => function ($q) {
             $q->orderBy('sort_order');
         }]);
-        return response()->json($people);
+        return response()->json(new PeopleResource($people));
     }
 
     public function update(UpdatePeopleRequest $request, People $people)
     {
         $people->update($request->validated());
-        return response()->json($people);
+        $people->load(['pictures' => function ($q) { $q->orderBy('sort_order'); }]);
+        return response()->json(new PeopleResource($people));
     }
 
     public function destroy(People $people)
---
+++ app/Http/Resources/PeopleResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PeopleResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => 'op'.(string) $this->id,
+            'name' => $this->name,
+            'age' => $this->age,
+            'bio' => $this->bio,
+            'distance' => $this->location,
+            'photos' => $this->whenLoaded('pictures', function () {
+                return $this->pictures->sortBy('sort_order')->pluck('photo_url')->values();
+            }, []),
+        ];
+    }
+}
+
