9d49bd216948a40e55fb87d8e447f384d88de880
khrsman <khrsman@gmail.com>
2025-11-21 22:03:56 +0700

    remove unused routes

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,11 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\PeopleController;
-use App\Http\Controllers\Api\PictureController;
 use App\Http\Controllers\Api\LikeController;
-use App\Http\Controllers\Api\LikedPeopleController;
 
 Route::prefix('people')->group(function () {
     Route::get('/', [PeopleController::class, 'index']);
     Route::post('/', [PeopleController::class, 'store']);
-    Route::get('{people}', [PeopleController::class, 'show']);
-    Route::put('{people}', [PeopleController::class, 'update']);
-    Route::delete('{people}', [PeopleController::class, 'destroy']);
-
-    Route::get('{people}/pictures', [PictureController::class, 'index']);
-    Route::post('{people}/pictures', [PictureController::class, 'store']);
-    Route::delete('{people}/pictures/{picture}', [PictureController::class, 'destroy']);
 });
 
 Route::prefix('likes')->group(function () {
@@ -23,5 +14,3 @@ Route::prefix('likes')->group(function () {
     Route::post('/', [LikeController::class, 'store']);
     Route::post('/rewind', [LikeController::class, 'rewind']);
 });
-
-Route::get('/liked-people', [LikedPeopleController::class, 'index']);
